10a1decbf0c814348a7885e70306a65c2ae0d723
Wim Lavrijsen <WLavrijsen@lbl.gov>
2023-10-17 14:51:27 -0700

    extend and cleanup pdarray construction tests based on examples in the manual

--- tests/array_construction_test.py
+++ tests/array_construction_test.py
@@ -4,6 +4,7 @@ from base_test import ArkoudaTest
 from context import arkouda as ak
 
 import arkjit
+import inspect
 
 
 class ArrayConstructionTests(ArkoudaTest):
@@ -17,48 +18,116 @@ class ArrayConstructionTests(ArkoudaTest):
 
         assert sum(res0.to_ndarray() == res1.to_ndarray()) == len(res0)
 
-    def test_arange(self):
-        """Test JITing of arkouda.arange"""
+    def verify(self, stack):
+        if isinstance(stack, dict):
+            stack = stack.values()
 
-        def calc1():
-            A = ak.arange(10)
-            return A
-
-        self.compare(calc1)
-
-        def calc2():
-            A = ak.arange(10, dtype=ak.float64)
-            return A
-
-        self.compare(calc2)
-
-        def calc3():
-            A = ak.arange(4, 40, 2)
-            return A
-
-        def calc4():
-            A = ak.arange(4, 40, 2, dtype=ak.float64)
-            return A
-
-        for c in [calc1, calc2, calc3, calc4]:
-             self.compare(c)
+        for count, c in enumerate((f for f in stack if inspect.isfunction(f)), 1):
+            self.compare(c)
+        return count
 
     def test_from_list(self):
-        """Test construction of pdarrays from Python lists"""
+        """JITing of pdarray construction from Python lists"""
 
-        def calc1():
+        def aj_calc1():
             l = [0, 1, 2, 3, 4]
-            A = ak.array(l)
-            return A
+            return ak.array(l)
 
-        self.compare(calc1)
+        assert self.verify(locals())
 
     def test_from_numpy(self):
-        """Test construction of pdarrays from numpy arrays"""
+        """JITing of pdarray construction from numpy arrays"""
 
         def calc1():
             a = np.array([0, 1, 2, 3, 4])
-            A = ak.array(a)
-            return A
+            return ak.array(a)
 
-        self.compare(calc1)
+        assert self.verify(locals())
+
+    def test_constants(self):
+        """JITing of constant constructors"""
+
+        def calc1():
+            return ak.zeros(5, dtype=ak.int64)
+
+        def calc1a():
+            zeros = ak.zeros(5, dtype=ak.int64)
+            return ak.zeros_like(zeros)
+
+        def calc2():
+            return ak.zeros(5, dtype=ak.float64)
+
+        def calc3a():
+            zeros = ak.zeros(5, dtype=ak.float64)
+            return ak.zeros_like(zeros)
+
+        def calc3():
+            return ak.zeros(5, dtype=ak.bool)
+
+        def calc3a():
+            zeros = ak.zeros(5, dtype=ak.bool)
+            return ak.zeros_like(zeros)
+
+        def calc4():
+            return ak.ones(5, dtype=ak.int64)
+
+        def calc4a():
+            ones = ak.ones(5, dtype=ak.int64)
+            return ak.ones_like(ones)
+
+        def calc5():
+            return ak.ones(5, dtype=ak.float64)
+
+        def calc5a():
+            ones = ak.ones(5, dtype=ak.float64)
+            return ak.ones_like(ones)
+
+        def calc6():
+            return ak.ones(5, dtype=ak.bool)
+
+        def calc6a():
+            ones = ak.ones(5, dtype=ak.bool)
+            return ak.ones_like(ones)
+
+        assert self.verify(locals())
+
+    def test_arange(self):
+        """JITing of arkouda.arange"""
+
+        def calc1():
+            return ak.arange(10)
+
+        def calc1a():
+            return ak.arange(10, dtype=ak.float64)
+
+        def calc2():
+            return ak.arange(0, 5, 1)
+
+        def calc3():
+            return ak.arange(5, 0, -1)
+
+        def calc4():
+            return ak.arange(0, 10, 2)
+
+        def calc4a():
+            return ak.arange(0, 10, 2, dtype=ak.float64)
+
+        def calc5():
+            return ak.arange(-5, -10, -1)
+
+        assert self.verify(locals())
+
+    def test_linspace(self):
+        """JITing of arkouda.linspace"""
+
+        def calc1():
+            return ak.linspace(0, 1, 5)
+
+        #def calc2():
+        #    return ak.linspace(start=1, stop=0, length=5)
+
+        #def calc3():
+        #    return ak.linspace(start=-5, stop=0, length=5)
+
+        assert self.verify(locals())
+
