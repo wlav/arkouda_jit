39e25c8b6aedc403121280d1557a575e2d4d2e19
Wim Lavrijsen <WLavrijsen@lbl.gov>
2023-10-27 12:03:44 -0700

    allow float64 numbers to differ in the 15th digit

--- tests/common.py
+++ tests/common.py
@@ -3,6 +3,7 @@ from base_test import ArkoudaTest
 import arkouda as ak
 import arkjit
 import inspect
+import numpy as np
 
 
 class ArkoudaJITTest(ArkoudaTest):
@@ -17,7 +18,10 @@ class ArkoudaJITTest(ArkoudaTest):
         if isinstance(res0, (ak.pdarray, ak.Strings)):
             assert sum(res0.to_ndarray() == res1.to_ndarray()) == len(res0)
         else:
-            assert res0 == res1
+            if type(res0) == np.float64:
+                assert round(res0-res1, 14) == 0
+            else:
+                assert res0 == res1
 
     def verify(self, stack):
         if isinstance(stack, dict):
