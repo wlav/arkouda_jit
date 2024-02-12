5c37903fb362b5d82c024c63226d687e6785b50c
Wim Lavrijsen <WLavrijsen@lbl.gov>
2024-02-12 11:26:11 -0800

    move auto-compilation earlier into the chain by running an untype function pass instead of typing untyped functions in the annotator

--- python/arkjit/compiler.py
+++ python/arkjit/compiler.py
@@ -6,86 +6,15 @@ import sys
 
 import numba.core.compiler as nb_cmp
 import numba.core.decorators as nb_dec
-import numba.core.dispatcher as nb_disp
 import numba.core.typed_passes as nb_typed_pass
-import numba.core.types as nb_types
 import numba.core.untyped_passes as nb_untyped_pass
-import numba.extending as nb_ext
 
-from .passes import ArkoudaCSE, ArkoudaFunctionPass
+from .passes import (AutoFunctionInlinerPass,
+                     ArkoudaCSE,
+                     ArkoudaFunctionPass,
+                    )
 
 __all__ = ["optimize", "ArkoudaCompiler"]
-
-
-# -- general function inliner -----------------------------------------------
-def _dummy():
-    pass
-
-
-class InlineUndefinedFunctionType(nb_types.UndefinedFunctionType):
-    def __init__(self, dispatcher):
-        pysig = inspect.signature(dispatcher.py_func)
-        super(InlineUndefinedFunctionType, self).__init__(len(pysig.parameters), [dispatcher])
-
-
-class CompilerState:
-    def __init__(self, target, typer, state):
-        self.target = target
-        self.typer = typer
-        self.state = state
-
-    def __enter__(self):
-        self.typer.state = self.state
-        if self.typer.top is None:
-            self.typer.top = self.target
-
-    def __exit__(self, tp, val, trace):
-        self.typer.top = None
-        self.typer.state = None
-
-
-class TypeofInlineFunction:
-    """Automatically inline helpers written on the interactive prompt."""
-
-    def __init__(self):
-        self._is_active = False
-        self.state = None
-        self.top = None
-
-    def __enter__(self):
-        self._is_active = True
-
-    def __exit__(self, tp, val, trace):
-        self._is_active = False
-
-    def __call__(self, val, c):
-        # TODO: not every function can be auto-inlined, and functions may
-        # have overloads instead. Therefore, restrict inline to only the
-        # main module (interactive use) or functions in the same module as
-        # the current function being JITed.
-        if not self._is_active or (self.top and\
-               (val.__module__ != "__main__" and val.__module__ != self.top.__module__)):
-            return None
-
-        # check whether the function has already been replaced (we get here
-        # for each call of the function in the closure being compiled)
-        disp = getattr(sys.modules[val.__module__], val.__name__)
-        if isinstance(disp, nb_disp.Dispatcher):
-             return nb_types.Dispatcher(disp)
-
-        # register dispatcher with Numba (will compile on use)
-        disp = optimize(inline="always")(val)
-
-        # place the dispatcher back into the module to ensure next call goes
-        # through the normal lookup, withing entering this fallback
-        setattr(sys.modules[val.__module__], val.__name__, disp)
-
-        # type this function as if it was always a dispatcher (JIT wrapper)
-        return nb_types.Dispatcher(disp)
-
-
-typeof_inline_function = TypeofInlineFunction()
-nb_ext.typeof_impl.register(type(_dummy))(typeof_inline_function)
 
 
 # -- compiler which adds custom Arkouda passes ------------------------------
@@ -101,16 +30,7 @@ class ArkoudaCompiler(nb_cmp.CompilerBase):
         self.cse_pass      = enable_all or "cse" in self.opt_passes
         self.function_pass = enable_all or "function" in self.opt_passes
 
-        self.force_inline  = enable_all or "inline" in self.opt_passes
-
-
-    def compile_extra(self, func):
-        if self.force_inline:
-            with typeof_inline_function, CompilerState(func, typeof_inline_function, self.state):
-                result = super(ArkoudaCompiler, self).compile_extra(func)
-        else:
-            result = super(ArkoudaCompiler, self).compile_extra(func)
-        return result
+        self.auto_compile  = enable_all or "auto" in self.opt_passes
 
     def define_pipelines(self):
         # start with the complete Numba pipeline
@@ -119,6 +39,12 @@ class ArkoudaCompiler(nb_cmp.CompilerBase):
         # function pass example
         if self.function_pass:
             pm.add_pass_after(ArkoudaFunctionPass, nb_untyped_pass.IRProcessing)
+
+        if self.auto_compile:
+            # there's no "add_pass_before" function; WithLifting is right
+            # before InlineClosureLikes, which needs to see the compiled result
+            # for (optional) inlining to work, so use that instead
+            pm.add_pass_after(AutoFunctionInlinerPass, nb_untyped_pass.WithLifting)
 
         # common subexpression elimination, first pass after typing is done
         if self.cse_pass:
@@ -140,9 +66,10 @@ def optimize(*args, **kwds):
         # there's no good way to communicate additional options through the
         # Numba compiler definition, this global setting will have to do
         ArkoudaCompiler.opt_passes = opt_passes
-        if "inline" in opt_passes and not "inline" in kwds:
-            kwds["inline"] = "always"
         del kwds["passes"]
+
+    # use inlining by default
+    kwds["inline"] = kwds.get("inline", "always")
 
     # use the Arkouda compiler and force nopython mode (soon the only choice)
     kwds["pipeline_class"] = ArkoudaCompiler
--- python/arkjit/passes.py
+++ python/arkjit/passes.py
@@ -1,13 +1,18 @@
 """ Arkouda-specific passes for Numba
 """
 
+import types as pytypes
+
 import numba.core.compiler_machinery as nb_cpl
+import numba.core.ir as nb_ir
+import numba.core.ir_utils as nb_iru
 
 from .numba_ext import PDArrayBinOpSignature
 
 __all__ = [
     "ArkoudaFunctionPass",
     "ArkoudaCSE",
+    "AutoFunctionInlinerPass",
 ]
 
 
@@ -25,6 +30,41 @@ class ArkoudaFunctionPass(nb_cpl.FunctionPass):
 
     def __str__(self):
         return "Arkouda function pass"
+
+
+@nb_cpl.register_pass(mutates_CFG=True, analysis_only=False)
+class AutoFunctionInlinerPass(nb_cpl.FunctionPass):
+    _name = "arkjit_auto_inliner"
+
+    def __init__(self):
+        nb_cpl.FunctionPass.__init__(self)
+
+    def run_pass(self, state):
+        """Automatically compile closures; with forced inline.
+        """
+
+        modified = False
+
+        work_list = list(state.func_ir.blocks.items())
+        completed = dict()
+        while work_list:
+            _label, block = work_list.pop()
+            for i, instr in enumerate(block.body):
+                if isinstance(instr, nb_ir.Assign):
+                    expr = instr.value
+                    if isinstance(expr, nb_ir.Expr) and expr.op == 'call':
+                        func_def = nb_iru.get_definition(state.func_ir, expr.func)
+                        if isinstance(func_def, nb_ir.Global):
+                            if isinstance(func_def.value, pytypes.FunctionType):
+                                if not func_def.value in completed:
+                                    from arkjit.compiler import optimize
+                                    disp = optimize(inline="always")(func_def.value)
+                                    completed[func_def.value] = disp
+
+                                func_def.value = completed[func_def.value]
+                                modified = True
+
+        return modified
 
 
 # -- block passes -----------------------------------------------------------
