0b9a1fd9426ae492f7875b8c8b93bac580b2c269
Wim Lavrijsen <WLavrijsen@lbl.gov>
2024-02-12 17:16:08 -0800

    refactor arkouda operations counting into the common test base class

--- tests/common.py
+++ tests/common.py
@@ -9,11 +9,22 @@ import numpy as np
 class ArkoudaJITTest(ArkoudaTest):
     """Base class for JIT tests"""
 
+    binop_counter = 0
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.counts = dict()
+
     def compare(self, passes, forg, *args, **kwds):
+        self.reset_counters()
         res0 = forg(*args, **kwds)
+        self.record_counters(forg, optimized=False)
 
         fopt = arkjit.optimize(passes=passes)(forg)
+
+        self.reset_counters()
         res1 = fopt(*args, **kwds)
+        self.record_counters(forg, optimized=True)
 
         if isinstance(res0, (ak.pdarray, ak.Strings)):
             assert sum(res0.to_ndarray() == res1.to_ndarray()) == len(res0)
@@ -23,10 +34,31 @@ class ArkoudaJITTest(ArkoudaTest):
             else:
                 assert res0 == res1
 
+    def setup_class(cls):
+        ArkoudaJITTest._binop = ak.pdarray._binop
+
+    @staticmethod
+    def _count_binop(pda, *args, **kwds):
+        ArkoudaJITTest.binop_counter += 1
+        return ArkoudaJITTest._binop(pda, *args, **kwds)
+
+    def setup_method(self, meth):
+        ak.pdarray._binop = ArkoudaJITTest._count_binop
+
+    def teardown_method(self, meth):
+        ak.pdarray._binop = ArkoudaJITTest._binop
+
+    def reset_counters(self):
+        ArkoudaJITTest.binop_counter = 0
+
+    def record_counters(self, func, optimized):
+        self.counts[(func, 'binop', optimized)] = ArkoudaJITTest.binop_counter
+
     def verify(self, stack, passes="all"):
         if isinstance(stack, dict):
             stack = stack.values()
 
-        for count, c in enumerate((f for f in stack if inspect.isfunction(f)), 1):
-            self.compare(passes, c)
-        return count
+        for i, func in enumerate((f for f in stack if inspect.isfunction(f)), 1):
+            self.compare(passes, func)
+
+        return i
--- tests/pass_test.py
+++ tests/pass_test.py
@@ -4,30 +4,13 @@ from common import ArkoudaJITTest
 from context import arkouda as ak
 
 
-# helper to test auto comilation/inlining
+# helper to test auto compilation/inlining
 def sqr(A):
     return A*A
 
 
 class PassTests(ArkoudaJITTest):
     """Test JITed pdarray set operations"""
-
-    def setup_class(cls):
-        PassTests.binop_counter = 0
-        PassTests._binop = ak.pdarray._binop
-
-    @staticmethod
-    def _count_binop(pda, *args, **kwds):
-        PassTests.binop_counter += 1
-        return PassTests._binop(pda, *args, **kwds)
-
-    def setup_method(self, meth):
-        PassTests.binop_counter = 0
-        ak.pdarray._binop = PassTests._count_binop
-
-    def teardown_method(self, meth):
-        ak.pdarray._binop = PassTests._binop
-        PassTests.binop_counter = 0
 
     def test_cse(self):
         """Common subexpression elimination"""
@@ -39,18 +22,30 @@ class PassTests(ArkoudaJITTest):
 
         assert self.verify(locals(), passes=("cse",))
 
-        assert PassTests.binop_counter == \
-                 3 + 2            # calc1
+        assert self.counts[(calc1, 'binop', True)] == 2
 
     def test_auto_inline(self):
         """Inline functions to enable CSE"""
 
         def calc1():
             A = ak.arange(10)
-            B = sqr(A) + A*A #sqr(A)
+            B = sqr(A) + A*A
+            return B
+
+        def calc2():
+            A = ak.arange(10)
+            B = A*A + sqr(A)
+            return B
+
+        def calc3():
+            A = ak.arange(10)
+            B = A*A + sqr(A)
             return B
 
         assert self.verify(locals(), passes=("auto", "cse",))
 
-        assert PassTests.binop_counter == \
-                 3 + 2            # calc1
+        assert self.counts[(calc1, 'binop', False)] == 3
+
+        assert self.counts[(calc1, 'binop', True)] == 2
+        assert self.counts[(calc2, 'binop', True)] == 2
+        assert self.counts[(calc3, 'binop', True)] == 2
