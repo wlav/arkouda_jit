60b91de2f2e8a8cfbed0d0d7a7ee12d8e683c073
Wim Lavrijsen <WLavrijsen@lbl.gov>
2024-03-18 21:50:43 -0700

    add a custom pass to handle pdarray lifetimes

--- python/arkjit/compiler.py
+++ python/arkjit/compiler.py
@@ -5,19 +5,94 @@ import inspect
 import sys
 
 import numba.core.compiler as nb_cmp
+import numba.core.compiler_machinery as nb_mach
 import numba.core.decorators as nb_dec
 import numba.core.typed_passes as nb_typed_pass
 import numba.core.untyped_passes as nb_untyped_pass
 
 from .passes import (AutoFunctionInlinerPass,
                      ArkoudaCSE,
+                     ArkoudaDel,
                      ArkoudaFunctionPass,
+                     NativeLoweringWithDel,
+                     NativeParforLoweringWithDel,
                      )
 
 __all__ = ["optimize", "ArkoudaCompiler"]
 
 
 # -- compiler which adds custom Arkouda passes ------------------------------
+class ArkoudaPassBuilder(nb_cmp.DefaultPassBuilder):
+    """
+    Redefines the lowering_pipeline to support DecRef/IncRef instructions;
+    these methods are otherwise the same as those from the DefaultPassBuilder.
+    """
+
+    # Copyright (c) 2012, Anaconda, Inc.
+    # All rights reserved.
+    #
+    # Redistribution and use in source and binary forms, with or without
+    # modification, are permitted provided that the following conditions are
+    # met:
+    #
+    # Redistributions of source code must retain the above copyright notice,
+    # this list of conditions and the following disclaimer.
+    #
+    # Redistributions in binary form must reproduce the above copyright
+    # notice, this list of conditions and the following disclaimer in the
+    # documentation and/or other materials provided with the distribution.
+    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    # A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    # HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    # LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    # DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    @staticmethod
+    def define_nopython_pipeline(state, name='nopython'):
+        """Returns an nopython mode pipeline based PassManager
+        """
+        # compose pipeline from untyped, typed and lowering parts
+        apb = ArkoudaPassBuilder
+        pm = nb_mach.PassManager(name)
+        untyped_passes = apb.define_untyped_pipeline(state)
+        pm.passes.extend(untyped_passes.passes)
+
+        typed_passes = apb.define_typed_pipeline(state)
+        pm.passes.extend(typed_passes.passes)
+
+        lowering_passes = apb.define_nopython_lowering_pipeline(state)
+        pm.passes.extend(lowering_passes.passes)
+
+        pm.finalize()
+        return pm
+
+    @staticmethod
+    def define_nopython_lowering_pipeline(state, name='nopython_lowering'):
+        pm = nb_mach.PassManager(name)
+        # legalise
+        pm.add_pass(nb_typed_pass.NoPythonSupportedFeatureValidation,
+                    "ensure features that are in use are in a valid form")
+        pm.add_pass(nb_typed_pass.IRLegalization,
+                    "ensure IR is legal prior to lowering")
+        # Annotate only once legalized
+        pm.add_pass(nb_typed_pass.AnnotateTypes, "annotate types")
+        # lower
+        if state.flags.auto_parallel.enabled:
+            pm.add_pass(NativeParforLoweringWithDel, "native parfor lowering with del")
+        else:
+            pm.add_pass(NativeLoweringWithDel, "native lowering with del")
+        pm.add_pass(nb_typed_pass.NoPythonBackend, "nopython mode backend")
+        pm.add_pass(nb_typed_pass.DumpParforDiagnostics, "dump parfor diagnostics")
+        pm.finalize()
+        return pm
+
+
 class ArkoudaCompiler(nb_cmp.CompilerBase):
     opt_passes = "all"
 
@@ -34,7 +109,7 @@ class ArkoudaCompiler(nb_cmp.CompilerBase):
 
     def define_pipelines(self):
         # start with the complete Numba pipeline
-        pm = nb_cmp.DefaultPassBuilder.define_nopython_pipeline(self.state)
+        pm = ArkoudaPassBuilder.define_nopython_pipeline(self.state)
 
         # function pass example
         if self.function_pass:
@@ -46,9 +121,16 @@ class ArkoudaCompiler(nb_cmp.CompilerBase):
             # for (optional) inlining to work, so use that instead
             pm.add_pass_after(AutoFunctionInlinerPass, nb_untyped_pass.WithLifting)
 
+        last_typed_arkouda_pass = nb_typed_pass.AnnotateTypes
+
         # common subexpression elimination, first pass after typing is done
         if self.cse_pass:
             pm.add_pass_after(ArkoudaCSE, nb_typed_pass.AnnotateTypes)
+            last_typed_arkouda_pass = ArkoudaCSE
+
+        # required pass to ensure proper ref-counting on Arkouda types (this
+        # has to be the last typed pass after all Arkouda-specific passes)
+        pm.add_pass_after(ArkoudaDel, last_typed_arkouda_pass)
 
         pm.finalize()
         return [pm]
--- python/arkjit/passes.py
+++ python/arkjit/passes.py
@@ -6,6 +6,12 @@ import types as pytypes
 import numba.core.compiler_machinery as nb_cpl
 import numba.core.ir as nb_ir
 import numba.core.ir_utils as nb_iru
+import numba.core.lowering as nb_lower
+import numba.core.typed_passes as nb_typed_pass
+import numba.core.types as nb_types
+import numba.parfors.parfor_lowering as nb_parfor_lower
+
+from llvmlite import ir
 
 from .numba_ext import (PDArrayType,
                         PDArrayBinOpSignature,
@@ -14,8 +20,33 @@ from .numba_ext import (PDArrayType,
 __all__ = [
     "ArkoudaFunctionPass",
     "ArkoudaCSE",
+    "ArkoudaDel",
     "AutoFunctionInlinerPass",
+    "NativeLoweringWithDel",
+    "NativeParforLoweringWithDel",
 ]
+
+
+# -- helpers ----------------------------------------------------------------
+class DecRef(nb_ir.Stmt):
+    def __init__(self, value, loc):
+        assert isinstance(value, str)
+        assert isinstance(loc, nb_ir.Loc)
+        self.value = value
+        self.loc = loc
+
+    def __str__(self):
+        return "decref %s" % self.value
+
+class IncRef(nb_ir.Stmt):
+    def __init__(self, value, loc):
+        assert isinstance(value, str)
+        assert isinstance(loc, nb_ir.Loc)
+        self.value = value
+        self.loc = loc
+
+    def __str__(self):
+        return "incref %s" % self.value
 
 
 # -- function passes --------------------------------------------------------
@@ -71,6 +102,93 @@ class AutoFunctionInlinerPass(nb_cpl.FunctionPass):
 
 # -- block passes -----------------------------------------------------------
 @nb_cpl.register_pass(mutates_CFG=True, analysis_only=False)
+class ArkoudaDel(nb_cpl.LoweringPass):
+    """
+    Explicit deletion of Arkouda objects
+
+    Numba ignores del statements b/c it's not possible to know when to call
+    them, since the trace normally operates on unboxed objects. Since pdarrays
+    remain boxed, normal ref-counting will have the destructor called as
+    appropriate by re-inserting decrefs for dels.
+
+    There is one exception to this: a returned object will be deleted in the
+    block
+
+    This pass requires the lowering pass to support the DecRef instruction.
+    """
+
+    _name = "arkouda_del"
+
+    def __init__(self):
+        super().__init__()
+
+    def _incref(self, ref, instr, state, newbody):
+        arg = state.typemap.get(ref, None)
+        if isinstance(arg, PDArrayType):
+            newbody.append(IncRef(ref, instr.loc))
+            return True
+        return False
+
+    def run_pass(self, state):
+        print("Arkouda explicit deletion pass")
+
+        modified = False
+
+        for block in state.func_ir.blocks.values():
+            newbody = list()
+            deferred = dict()
+            for instr in block.body:
+                if isinstance(instr, nb_ir.Del):
+                    ref = instr.value
+                    arg = state.typemap.get(ref, None)
+                    if isinstance(arg, PDArrayType):
+                        newbody.append(DecRef(ref, instr.loc))
+                        modified = True
+                    elif instr.value in deferred:
+                        for defer in reversed(deferred[instr.value]):
+                            newbody.append(DecRef(defer, instr.loc))
+                        del deferred[instr.value]
+                        newbody.append(instr)
+
+                elif isinstance(instr, nb_ir.Assign):
+                    # References can be borrowed/stolen if used in certain ops (this
+                    # happens for assignments as the lhs needs to persist beyond the
+                    # call for this to matter). In principle, decref should only be
+                    # deferred, but it is simpler to pass the reference explicitly,
+                    # as the borrower will eventually be deleted as well.
+                    expr = instr.value
+                    if isinstance(expr, nb_ir.Expr):
+                        if expr.op == "cast" or expr.op == "getattr":
+                            if self._incref(expr.value.name, instr, state, newbody):
+                                modified = True
+                        elif expr.op == "build_list":
+                            to_defer = list()
+                            for item in expr.items:
+                                if self._incref(item.name, instr, state, newbody):
+                                    to_defer.append(item.name)
+                                    modified = True
+                            if to_defer:
+                                deferred[instr.target.name] = to_defer
+
+                    elif isinstance(expr, nb_ir.Var):
+                        if self._incref(expr.name, instr, state, newbody):
+                            modified = True
+
+                    newbody.append(instr)
+
+                else:
+                    newbody.append(instr)
+
+        if modified:
+            block.body = newbody
+
+        return modified
+
+    def __str__(self):
+        return "lifetime management for PDArrays"
+
+
+@nb_cpl.register_pass(mutates_CFG=True, analysis_only=False)
 class ArkoudaCSE(nb_cpl.LoweringPass):
     """
     Common Subexpression Elmination (CSE) for Arkouda operations
@@ -122,3 +240,54 @@ class ArkoudaCSE(nb_cpl.LoweringPass):
 
     def __str__(self):
         return "common subexpression elimination for PDArrays"
+
+
+# -- lowering passes --------------------------------------------------------
+def _handle_decref(self, inst):
+    pyapi = self.context.get_python_api(self.builder)
+    value = self.loadvar(inst.value)
+    pyapi.decref(value)
+
+def _handle_incref(self, inst):
+    pyapi = self.context.get_python_api(self.builder)
+    value = self.loadvar(inst.value)
+    pyapi.incref(value)
+
+class LowerWithDel(nb_lower.Lower):
+    def lower_inst(self, inst):
+        if isinstance(inst, DecRef):
+            return _handle_decref(self, inst)
+        elif isinstance(inst, IncRef):
+            return _handle_incref(self, inst)
+        return super().lower_inst(inst)
+
+@nb_cpl.register_pass(mutates_CFG=True, analysis_only=False)
+class NativeLoweringWithDel(nb_typed_pass.NativeLowering):
+    """Add explicit deletion of Arkouda objects to NativeLowering.
+    """
+
+    _name = "native_lowering_with_del"
+
+    @property
+    def lowering_class(self):
+        return LowerWithDel
+
+
+class ParForLowerWithDel(nb_parfor_lower.ParforLower):
+    def lower_inst(self, inst):
+        if isinstance(inst, DecRef):
+            return _handle_decref(self, inst)
+        elif isinstance(inst, IncRef):
+            return _handle_incref(self, inst)
+        return super().lower_inst(inst)
+
+@nb_cpl.register_pass(mutates_CFG=True, analysis_only=False)
+class NativeParforLoweringWithDel(nb_typed_pass.NativeParforLowering):
+    """Adds explicit deletion of Arkouda objects to NativeParforLowering.
+    """
+
+    _name = "native_parfor_lowering_with_del"
+
+    @property
+    def lowering_class(self):
+        return ParForLowerWithDel
